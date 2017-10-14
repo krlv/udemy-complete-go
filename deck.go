b5ecb184c03e0f9ef87fdfd959b16609ca96a6bd
krlv <eug.krlv@gmail.com>
2017-10-14 14:47:09 +0300

    Refactor suite & rank entities to types

--- deck.go
+++ deck.go
@@ -2,10 +2,52 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"time"
 	"math/rand"
+	"time"
 )
+
+// Type Rank is a numeric representation of a card's rank
+type Rank int
+
+const (
+	_ Rank = iota
+	_
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+	Ace
+)
+
+func (r Rank) String() string {
+	return [...]string{
+		"", "", "Two", "Three", "Four", "Five",
+		"Six", "Seven", "Eight", "Nine", "Ten",
+		"Jack", "Queen", "King", "Ace",
+	}[r]
+}
+
+// Type Suite is a numeric representation of a card's suit
+type Suite int
+
+const (
+	Diamonds Suite = iota
+	Hearts
+	Clubs
+	Spades
+)
+
+func (s Suite) String() string {
+	return [...]string{"Diamonds", "Hearts", "Clubs", "Spades"}[s]
+}
 
 // Type Hand is a slice of cards
 type Hand []Card
@@ -15,47 +57,48 @@ type Deck []Card
 
 // Type Card ...
 type Card struct {
-	Rank string
-	Suit string
+	Rank  Rank
+	Suite Suite
 }
 
-func (c Card) Print () {
-	fmt.Println(strings.Title(c.Rank) + " of " + strings.Title(c.Suit))
+func (c Card) Print() {
+	fmt.Println(c.Rank.String() + " of " + c.Suite.String())
 }
 
-func (h Hand) Print () {
+func (h Hand) Print() {
 	for _, card := range h {
 		card.Print()
 	}
 }
 
-func (d Deck) NewCard () Card {
-	ranks := []string {
-		"three",
-		"four",
-		"five",
-		"six",
-		"seven",
-		"eight",
-		"nine",
-		"ten",
-		"jack",
-		"queen",
-		"king",
-		"ace",
+func (d Deck) NewCard() Card {
+	ranks := [...]Rank{
+		Two,
+		Three,
+		Four,
+		Five,
+		Six,
+		Seven,
+		Eight,
+		Nine,
+		Ten,
+		Jack,
+		Queen,
+		King,
+		Ace,
 	}
 
-	suits := []string {
-		"diamonds",
-		"hearts",
-		"clubs",
-		"spades",
+	suites := []Suite{
+		Diamonds,
+		Hearts,
+		Clubs,
+		Spades,
 	}
 
 	rand.Seed(time.Now().UnixNano())
 
 	return Card{
-		Rank: ranks[rand.Intn(len(ranks))],
-		Suit: suits[rand.Intn(len(suits))],
+		Rank:  ranks[rand.Intn(int(Ace))],
+		Suite: suites[rand.Intn(int(Spades))],
 	}
 }
