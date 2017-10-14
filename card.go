ac005458cf934af682ef0142f45076d707cff3a7
krlv <eug.krlv@gmail.com>
2017-10-14 14:55:46 +0300

    Move card logic to separate file

---
+++ card.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Type Rank is a numeric representation of a card's rank
+type Rank int
+
+const (
+	_ Rank = iota
+	_
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+	Ace
+)
+
+func (r Rank) String() string {
+	return [...]string{
+		"", "", "Two", "Three", "Four", "Five",
+		"Six", "Seven", "Eight", "Nine", "Ten",
+		"Jack", "Queen", "King", "Ace",
+	}[r]
+}
+
+// Type Suite is a numeric representation of a card's suit
+type Suite int
+
+const (
+	Diamonds Suite = iota
+	Hearts
+	Clubs
+	Spades
+)
+
+func (s Suite) String() string {
+	return [...]string{"Diamonds", "Hearts", "Clubs", "Spades"}[s]
+}
+
+// Type Card ...
+type Card struct {
+	Rank  Rank
+	Suite Suite
+}
+
+func (c Card) Print() {
+	fmt.Println(c.Rank.String() + " of " + c.Suite.String())
+}
+
+func (d Deck) NewCard() Card {
+	ranks := [...]Rank{
+		Two,
+		Three,
+		Four,
+		Five,
+		Six,
+		Seven,
+		Eight,
+		Nine,
+		Ten,
+		Jack,
+		Queen,
+		King,
+		Ace,
+	}
+
+	suites := []Suite{
+		Diamonds,
+		Hearts,
+		Clubs,
+		Spades,
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	return Card{
+		Rank:  ranks[rand.Intn(int(Ace))],
+		Suite: suites[rand.Intn(int(Spades))],
+	}
+}
