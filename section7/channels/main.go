3ccb7ce1139cdec9f018aaf01221fede72ddb9a5
krlv <eug.krlv@gmail.com>
2019-02-27 16:52:07 -0800

    L80: Checking URLs concurently, continuously

--- section7/channels/main.go
+++ section7/channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -13,16 +14,23 @@ func main() {
 		"https://amazon.com",
 	}
 
-	ch := make(chan bool)
+	ch := make(chan func() (string, bool))
 
-	for _, url := range urls {
-		go CheckURL(url, ch)
-	}
+	go func() {
+		for {
+			for _, url := range urls {
+				go CheckURL(url, ch)
+			}
 
-	for _, url := range urls {
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	for f := range ch {
+		url, st := f()
+
 		status := "not ok"
-
-		if <-ch {
+		if st {
 			status = "ok"
 		}
 
@@ -32,18 +40,24 @@ func main() {
 
 // CheckURL sends true to the channel if URL is ok (1XX, 2XX, 3XX status code),
 // otherwise sends false
-func CheckURL(url string, ch chan bool) {
+func CheckURL(url string, ch chan func() (string, bool)) {
 	resp, err := http.Get(url)
 	if err != nil {
-		ch <- false
+		ch <- getURLStatusClosure(url, false)
 		return
 	}
 
 	if resp.StatusCode > 399 {
-		ch <- false
+		ch <- getURLStatusClosure(url, false)
 		return
 	}
 
-	ch <- true
+	ch <- getURLStatusClosure(url, true)
 	return
+}
+
+func getURLStatusClosure(url string, status bool) func() (string, bool) {
+	return func() (string, bool) {
+		return url, status
+	}
 }
